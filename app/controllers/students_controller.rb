1b96fee0a0e74899cb09ee83bc905cb1e88b3095
Ayumi Fukuda Bennett <ayumi.fukuda@gmail.com>
2015-06-07 09:09:58 -0500

    Names are populated in a option box

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,4 +1,5 @@
 class StudentsController < ApplicationController
   def index
+    @students = Student.order(:first_name)
   end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,4 @@
+
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
 #
@@ -5,3 +6,23 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+Student.create(first_name: "Dotun", last_name: "Olusoga")
+Student.create(first_name: "Brandon", last_name: "Espinoza")
+Student.create(first_name: "Ayumi", last_name: "Bennett")
+Student.create(first_name: "David", last_name: "Shoup")
+Student.create(first_name: "Luke", last_name: "Lancaster")
+Student.create(first_name: "Andy", last_name: "Meagher")
+Student.create(first_name: "Margaret", last_name: "Clinard")
+Student.create(first_name: "Brandon", last_name: "Howard")
+Student.create(first_name: "Devin", last_name: "Readel")
+Student.create(first_name: "Tyler", last_name: "Black")
+Student.create(first_name: "Allison", last_name: "Higginbotham")
+Student.create(first_name: "Matthew", last_name: "Reuther")
+Student.create(first_name: "Erin", last_name: "Page")
+Student.create(first_name: "Molly", last_name: "Black")
+Student.create(first_name: "Kyle", last_name: "McCormick")
+Student.create(first_name: "Bryan", last_name: "Finlayson")
+Student.create(first_name: "Ed", last_name: "Dean")
+Student.create(first_name: "Jacob", last_name: "Lewis")
+Student.create(first_name: "Kayte", last_name: "Russell")
+Student.create(first_name: "Austin", last_name: "Erlandson")
